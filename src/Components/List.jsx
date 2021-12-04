61d010d814987df78a01ad58ae956a193b7d24a8
Saurabh Indoria <saurabhindoria@Saurabhs-Macbook-Pro.local>
2021-12-04 08:48:16 +0530

    fade

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,12 +1,14 @@
 import shortid from 'shortid';
-function List({list,handleToggle}){
-    
+function List({list,handleToggle,onDelete}){
+     
     return(
         <div>
         <h4>list</h4>
         <ul className="list-group list-group-flush">
             { list.map((i) => 
-            <li className= "list-group-item d-flex justify-content-between align-items-center " key = {shortid.generate()} id = {i.id} onClick = {(event) => handleToggle(event)}> { i.complete ? <del>{i.item}</del> : i.item } </li>
+            <li className= {i.complete ? "list-group-item d-flex justify-content-between align-items-center item-fadeout" : "list-group-item d-flex justify-content-between align-items-center "} key = {shortid.generate()} id = {i.id} onClick = {(event) => handleToggle(event)}> {i.item } 
+            <button className="btn btn-primary" onClick = {onDelete}>Clear Completed Task</button>
+            </li>
             )}
         </ul>
         </div>
