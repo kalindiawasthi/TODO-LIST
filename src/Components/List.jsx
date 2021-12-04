cbd76b19da485545a88f22c90e22a0919deafdfd
kalindiawasthi <63408822+kalindiawasthi@users.noreply.github.com>
2021-12-04 10:55:46 +0530

    transition-fadeout

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -4,7 +4,7 @@ function List({list,handleToggle,onDelete}){
     return(
         <div>
         <h4>list</h4>
-        <ul className="list-group list-group-flush">
+        <ul className="list-group ">
             { list.map((i) => 
             <li className= {i.complete ? "list-group-item d-flex justify-content-between align-items-center item-fadeout" : "list-group-item d-flex justify-content-between align-items-center "} key = {shortid.generate()} id = {i.id} onClick = {(event) => handleToggle(event)}> {i.item } 
             <button className="btn btn-primary" onClick = {onDelete}>Clear Completed Task</button>
